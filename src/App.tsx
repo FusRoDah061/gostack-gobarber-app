200dc8307542a973ed62e41bf015acb494f2bdaa
Allex Rodrigues <allexxrodriguess@gmail.com>
2020-11-05 18:59:36 -0300

    Cofigurando CodePush

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import codePush from 'react-native-code-push';
 
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
@@ -17,4 +18,6 @@ const App: React.FC = () => (
   </NavigationContainer>
 );
 
-export default App;
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(App);
